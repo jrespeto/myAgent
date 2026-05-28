3198afc019d594a7019a499fc5a5e96cddfa9b45
Jonathan <jrespeto@gmail.com>
2026-05-28 17:53:24 +0000

    Add conditional setup for Hermes and OpenClaw agents during image build

--- skel/setup_hermes.sh
+++ skel/setup_hermes.sh
@@ -1,1 +1,16 @@
-curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --skip-setup --no-venv --dir /hermes/hermes-agent
+# If image setup 
+
+action=$1
+
+if [ "$action" == "image-build" ]; then
+    echo "Image build detected. Skipping Hermes agent setup."
+    curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --skip-setup --no-venv --dir /hermes/hermes-agent
+else
+    echo "Running Hermes agent setup."
+    curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --dir /hermes/hermes-agent
+fi
+
+
+
+
+
--- skel/setup_openclaw.sh
+++ skel/setup_openclaw.sh
@@ -1,1 +1,12 @@
-curl -fsSL --proto '=https' --tlsv1.2 https://openclaw.ai/install.sh | bash -s -- --no-prompt --install-method npm --no-onboard; \
+
+action=$1 # 'image-build'
+
+if [ "$action" == "image-build" ]; then
+    echo "Image build detected. Skipping OpenClaw agent setup."
+    curl -fsSL --proto '=https' --tlsv1.2 https://openclaw.ai/install.sh | bash -s -- --no-prompt --install-method npm --no-onboard; \
+else
+    echo "Try running:"
+    echo "openclaw onboard"
+
+fi
+
