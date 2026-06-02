252ee4d3e6bdc5d4aa3f535067879f6d6cf40bb6
Jonathan <jrespeto@gmail.com>
2026-06-02 20:07:38 +0000

    Remove desktop installation option from Hermes agent setup script

--- skel/setup_hermes.sh
+++ skel/setup_hermes.sh
@@ -4,10 +4,10 @@ action=$1
 
 if [ "$action" == "image-build" ]; then
     echo "Image build detected. Skipping Hermes agent setup."
-    curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --skip-setup --skip-browser --no-skills --dir /hermes/hermes-agent --non-interactive --include-desktop
+    curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --skip-setup --skip-browser --no-skills --dir /hermes/hermes-agent --non-interactive 
 else
     echo "Running Hermes agent setup."
-    curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --dir /hermes/hermes-agent --skip-setup --non-interactive --include-desktop
+    curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --dir /hermes/hermes-agent --skip-setup --non-interactive 
 fi
 
 
